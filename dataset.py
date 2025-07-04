69682a1a36e65a8017bae24ec34467dbd8ce9043
Abdallah Abu Zaid <157073122+Abdallah-Tarik14@users.noreply.github.com>
2025-07-04 17:41:50 +0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -12,13 +12,12 @@ from PIL import Image
 import glob
 import os
 
-def loader(path, batch_size=32, num_workers=4, pin_memory=True):
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+def loader(batch_size=32, num_workers=4, pin_memory=True):
+    normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
     return data.DataLoader(
-        datasets.ImageFolder(path,
-                             transforms.Compose([
-                                 transforms.Scale(256),
-                                 transforms.RandomSizedCrop(224),
+        datasets.CIFAR10(root='./data', train=True, download=True,
+                             transform=transforms.Compose([
+                                 transforms.RandomCrop(32, padding=4),
                                  transforms.RandomHorizontalFlip(),
                                  transforms.ToTensor(),
                                  normalize,
@@ -28,13 +27,11 @@ def loader(path, batch_size=32, num_workers=4, pin_memory=True):
         num_workers=num_workers,
         pin_memory=pin_memory)
 
-def test_loader(path, batch_size=32, num_workers=4, pin_memory=True):
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+def test_loader(batch_size=32, num_workers=4, pin_memory=True):
+    normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
     return data.DataLoader(
-        datasets.ImageFolder(path,
-                             transforms.Compose([
-                                 transforms.Scale(256),
-                                 transforms.CenterCrop(224),
+        datasets.CIFAR10(root='./data', train=False, download=True,
+                             transform=transforms.Compose([
                                  transforms.ToTensor(),
                                  normalize,
                              ])),
@@ -42,3 +39,5 @@ def test_loader(path, batch_size=32, num_workers=4, pin_memory=True):
         shuffle=False,
         num_workers=num_workers,
         pin_memory=pin_memory)
+
+
